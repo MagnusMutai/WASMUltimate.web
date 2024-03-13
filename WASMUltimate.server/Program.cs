9ab828404541160428a36305aa6490a6a9fa5c90
Magnus Mutai <MagnusMq07@gmail.com>
2024-03-13 12:03:32 +0300

    Migrations

--- WASMUltimate.server/Program.cs
+++ WASMUltimate.server/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using WASMUltimate.server.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
