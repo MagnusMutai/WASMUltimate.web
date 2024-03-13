ca3e1bd033f0c4a7c9e3ad8c6cea0514528e7bf3
Magnus Mutai <MagnusMq07@gmail.com>
2024-03-13 16:02:01 +0300

    Employee Controller in Controller in Server with Get method

---
+++ WASMUltimate.server/Controllers/EmployeesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using WASMUltimate.server.Models;
+using WASMUltra.Shared;
+
+namespace WASMUltimate.server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository; 
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+
+            try
+            {
+                return Ok(await employeeRepository.GetEmployees());
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+            
+        }
+    }
+}
