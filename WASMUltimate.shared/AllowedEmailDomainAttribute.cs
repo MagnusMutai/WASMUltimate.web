e18be5264693d62f8b21c0177bcb2eeaac04a393
Magnus Mutai <MagnusMq07@gmail.com>
2024-07-10 11:47:09 +0300

    Added email validation, firstname and lastname required and minlength validation(using both Validationattributes and Validationrules) in Employees Class and datagridbutton page. Added validationattributes to specify the header of datagrid column using Display (Name)

---
+++ WASMUltimate.shared/AllowedEmailDomainAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WASMUltimate.shared
+{
+    public class AllowedEmailDomainAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            
+        }
+
+    }
+}
