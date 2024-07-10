e1f1025acc29027ab12e9fd71127d1d7b6a405c9
Magnus Mutai <MagnusMq07@gmail.com>
2024-07-10 12:04:11 +0300

    Added custom validation inheriting form validation attribute to make sure the email domain is a specific domain

--- WASMUltimate.shared/AllowedEmailDomainAttribute.cs
+++ WASMUltimate.shared/AllowedEmailDomainAttribute.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,27 @@ namespace WASMUltimate.shared
     {
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
-            
+            if(value == null || value.ToString().Length == 0 )
+            {
+                return new ValidationResult("Email Address is required");
+            }
+            else
+            {
+                if(value.ToString().IndexOf('@') == -1)
+                {
+                    return new ValidationResult("Invalid Email Address");
+                }
+
+                string[] strings = value.ToString().Split('@');
+                //magnus@magnusq.com
+                if (strings[1].ToLower() == "magnusq.com" )
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult("Email domain must be magnusq.com")
+                }
         }
 
     }
